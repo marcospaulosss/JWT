02690e2c5b6b324872cad2ba8010dd33fa6a7229
Marcos paulo de sousa santos <marcospaulo.s.s@hotmail.com>
2018-06-18 15:38:51 -0300

    update gauth
    secret for parameters

--- gAuth.php
+++ gAuth.php
@@ -52,14 +52,14 @@ class gAuth
      * @see var gAuth::$cookies
      * @see var gAuth::$request
      */
-    public function __construct()
+    public function __construct($secret)
     {
         $this->token = new stdClass();
         $this->crypt = Di::getDefault()->get('crypt');
         $this->cookies = Di::getDefault()->get('cookies');
         $this->request = Di::getDefault()->get('request');
 
-        $this->secret = '&1qjeG[=?219~5^"4;/estrategiaT3#7';
+        $this->secret = $secret;
     }
 
     /**
